0d605c16c485a329ebe6047a744785b80e5eb617
Alex Kibler <alexkibler@me.com>
2026-01-09 11:14:04 -0700

    Add precomipled assets

---
+++ public/avo-assets/late-registration.js
@@ -0,0 +1,2 @@
+(()=>{var n=window.Stimulus;window.Avo.configuration.stimulus_controllers&&window.Avo.configuration.stimulus_controllers.forEach(([o,i])=>{n.register(o,window[i])});})();
+//# sourceMappingURL=late-registration.js.map
